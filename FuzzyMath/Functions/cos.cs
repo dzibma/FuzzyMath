fa1f7d8f257ae2c5baf2f99933f188926f9c5565
dzibma <martindzibela@gmail.com>
2015-12-16 11:50:01 +0100

    fix bug in cosine func

--- FuzzyMath/Functions/cos.cs
+++ FuzzyMath/Functions/cos.cs
@@ -44,7 +44,7 @@ namespace FuzzyMath
 
             if (x.Contains(extreme))
             {
-                b = Math.Sin(extreme);
+                b = Math.Cos(extreme);
             }
             else
             {
