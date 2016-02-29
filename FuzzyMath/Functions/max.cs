c42d9b4cfa6dcc8b618bc76e4d607de42ada4358
dzibma <martindzibela@gmail.com>
2016-02-29 20:17:39 +0100

    add Min and Max function

---
+++ FuzzyMath/Functions/max.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace FuzzyMath
+{
+    public static partial class Functions
+    {
+
+        public static FuzzyNumber Max(FuzzyNumber X, FuzzyNumber Y)
+        {
+            return FuzzyNumber.Map(X, Y, (x, y) => Max(x, y));
+        }
+
+        public static Interval Max(Interval x, Interval y)
+        {
+            return new Interval(Math.Max(x.A, y.A), Math.Max(x.B, y.B));
+        }
+
+    }
+}
---
+++ FuzzyMath/Functions/min.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace FuzzyMath
+{
+    public static partial class Functions
+    {
+
+        public static FuzzyNumber Min(FuzzyNumber X, FuzzyNumber Y)
+        {
+            return FuzzyNumber.Map(X, Y, (x, y) => Min(x, y));
+        }
+
+        public static Interval Min(Interval x, Interval y)
+        {
+            return new Interval(Math.Min(x.A, y.A), Math.Min(x.B, y.B));
+        }
+
+    }
+}
